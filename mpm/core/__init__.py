5f10e2718c9cef018a9b312ee4c2a47ab3ee45a4
dodo325 <dodo101000101@gmail.com>
2020-06-10 15:16:56 +0300

    fix(configs): PACKAGE_DIR

--- mpm/core/__init__.py
+++ mpm/core/__init__.py
@@ -4,6 +4,8 @@ from mpm import __file__ as mpm__file__
 
 
 PACKAGE_DIR = Path(mpm__file__).resolve().parent
+if PACKAGE_DIR.parts[-1].startswith('mpm') and PACKAGE_DIR.parts[-2].startswith('mpm'):
+    PACKAGE_DIR = PACKAGE_DIR.parent
 SCRIPTS_DIR = PACKAGE_DIR / "scripts"
 CONFIGS_DIR = PACKAGE_DIR / "configs"
 
