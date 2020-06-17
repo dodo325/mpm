a63d5ed547fd988996f7d477087149e0125cf5f3
dodo325 <dodo101000101@gmail.com>
2020-06-17 22:58:00 +0300

    docs(utils): add __docs__ in multiget

--- mpm/utils/json_parse.py
+++ mpm/utils/json_parse.py
@@ -1,7 +1,11 @@
 from typing import List
 
 
-def multiget(d: dict, keys: List[str], default):
+def multiget(d: dict, keys: List[str], default=None):
+    """
+    Вытаскивает из словоря key из keys, если его нет, то пытается со следующим. 
+    Если ничего не найденно то позвращает default
+    """
     for key in keys:
         val = d.get(key, default)
         if val != default:
