9942eea7d04e5508dd96625c41a25a32f4e469ff
dodo325 <dodo101000101@gmail.com>
2020-06-20 13:51:32 +0300

    test(cli): shells init

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,20 +1,25 @@
 # https://click.palletsprojects.com/en/7.x/testing/
 from click.testing import CliRunner
-from mpm.core.cli import info, main
+from mpm.core.cli import info, main, search
 import pytest
 
 # @pytest.mark("cli")
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_info():
     runner = CliRunner()
     result = runner.invoke(info, ["pytest"])
     assert result.exit_code == 0
     # assert result.output == 'Hello Peter!\n'
 
+def test_search():
+    runner = CliRunner()
+    result = runner.invoke(search, ["pytest"])
+    assert result.exit_code == 0
+    # assert result.output == 'Hello Peter!\n'
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_main():
     runner = CliRunner()
     result = runner.invoke(main)
