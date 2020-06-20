562c626a23c97ed3125d442351c1728022652cbd
dodo325 <dodo101000101@gmail.com>
2020-06-20 23:32:29 +0300

    test(config): add test_get_settings

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,7 +1,11 @@
-from mpm.core.configs import CONFIGS_DIR, user_known_packages_file, user_settings_file
+from mpm.core.configs import get_settings, CONFIGS_DIR, settings_file, known_packages_file
 import pytest
 
 def test_CONFIGS_DIR():
     assert CONFIGS_DIR.exists()
-    assert user_known_packages_file.is_file()
-    assert user_settings_file.is_file()
+    assert known_packages_file.is_file()
+    assert settings_file.is_file()
+
+def test_get_settings():
+    settings = get_settings()
+    assert "known_packages_url" in settings
