30c408bf896d081744a4d7f7cfc82445b46abe6b
dodo325 <dodo101000101@gmail.com>
2020-06-21 12:27:53 +0300

    test(cli): add test_version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,8 @@
 # https://click.palletsprojects.com/en/7.x/testing/
 from click.testing import CliRunner
-from mpm.core.cli import info, main, search
+from mpm.core.cli import info, main, search, version
 import pytest
-
+import mpm
 # @pytest.mark("cli")
 
 
@@ -20,8 +20,18 @@ import pytest
 #     # assert result.output == 'Hello Peter!\n'
 
 # # @pytest.mark.skip()
-# def test_main():
-#     runner = CliRunner()
-#     result = runner.invoke(main)
-#     assert result.exit_code == 0
-#     print(result.output)
+def test_main():
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+    print(result.output)
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+    print(result.output)
+
+def test_getAbout():
+    info = mpm.getAbout()
+    assert "Version" in info
